66ef402a44786ef0290596a629aea438c91595c8
Airet <thiefon@gmail.com>
2024-03-23 16:59:25 +0500

    Подклчюил vue-yandex-maps.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import './assets/scss/main.scss'
 
 import { createApp } from 'vue'
+import { createYmaps } from 'vue-yandex-maps'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(
+  createYmaps({
+    apikey: 'c562ba4f-30ad-4e15-bc38-3aaa3179cc1d'
+  })
+)
+
+app.mount('#app')
